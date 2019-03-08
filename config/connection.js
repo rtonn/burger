64f99ff777b128fdf1e3ac41a58a8008b6cc46aa
rtonn <rtonn09umn@gmail.com>
2019-03-07 22:37:25 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection;
 
 if(process.env.JAWSDB_URL){
-    connection=mysql.createConnection(process.env.JAWSDB_URL)
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
 }
 else{
     connection=mysql.createConnection({
@@ -12,8 +12,8 @@ else{
         user:"root",
         password:"root",
         database:"burgers_db"
-    })
-}
+    });
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
